0fba9e8230c659e66c28fd26223a4b0cdda9b9b9
SalihTasdelen <salih.tasdelen@hotmail.com>
2021-09-18 13:26:29 +0300

    Added SepBy Parser Test

---
+++ tests/test_sepby_parser.py
@@ -0,0 +1,29 @@
+import unittest
+from paco.combinators import (Char, Regex, SepBy)
+
+class TestSepByParser(unittest.TestCase):
+
+    def setUp(self):
+        self.integer = Regex(r'[1-9][0-9]*')
+        self.comm = Char(',') << Regex(r' *')
+        self.rule = SepBy(self.integer, self.comm)
+
+    def test_empty(self):
+        result = self.rule('')
+        err = self.integer('')
+        self.assertEqual(result.start, err.start)
+        self.assertEqual(result.end, err.end)
+        self.assertEqual(result.msg, err.msg)
+
+    def test_return(self):
+        text = '12, 15, 21, 42, 34'
+        self.assertEqual(self.rule(text),
+                        (len(text), ['12', '15', '21', '42', '34']))
+
+    def test_error(self):
+        text = '12, 15, '
+        result = self.rule(text)
+        err = self.integer(text,len(text))
+        self.assertEqual(result.start, err.start)
+        self.assertEqual(result.end, err.end)
+        self.assertEqual(result.msg, err.msg)
