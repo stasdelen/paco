4e0fa4a4e6f296ce5dab4cf1df80dcff3a6c2f62
SalihTasdelen <salih.tasdelen@hotmail.com>
2021-09-19 20:50:25 +0300

    Updated the Recursive Array Example

--- examples/recursive_array.py
+++ examples/recursive_array.py
@@ -1,22 +1,29 @@
 from paco.combinators import Lazy
 from paco.atomic import Char
-from paco.miscellaneous import (letters, numbers, optSpace)
+from paco.miscellaneous import (STRING, FLOAT, INTEGER, OWS)
 
-lbra = Char('[').then(optSpace)
-rbra = Char(']').then(optSpace)
-comm = Char(',').then(optSpace)
+lbra = Char('[') << OWS
+rbra = Char(']') << OWS
+comm = Char(',') << OWS
 
-array = Lazy().rename('array')
-element = numbers.rename('int') | letters.rename('str') | array
-array.p = optSpace >> lbra >> element.sepby(comm) << rbra << optSpace
+
+floatNum = FLOAT.map(lambda x : float(x))
+intNum = INTEGER.map(lambda x : int(x))
+
+array = Lazy()
+element = (floatNum | intNum | STRING | array) << OWS
+array.p = OWS >> lbra >> element.sepby(comm) << rbra
 
 def main():
-    test_str = ' [ [1, 3, 5], [hi, howdy, bye], 42, [[1,2], [4,5]]] '
+    test_str = " [ [1, 3, 2.57e-3], ['hi', 'howdy', 'bye'], 42.32, [ [1, 2], [4 , 5]] ] "
     print('Running on: ' + test_str)
     _, ar = array(test_str)
-    print('(ar[0][2] == {}) ->'.format(ar[0][2]), ar[0][2] == '5')
-    print('(ar[1][1] == {}) ->'.format(ar[1][1]), ar[1][1] == 'howdy')
-    print('(ar[3][1][0] == {}) ->'.format(ar[3][1][0]), ar[3][1][0] == '4')
+    print('Result: {}'.format(ar))
+    
+    print('(ar[0][2] == {})'.format(ar[0][2]))
+    print('(ar[1][1] == {})'.format(ar[1][1]))
+    print('(ar[2] == {})'.format(ar[2]))
+    print('(ar[3][1][0] == {})'.format(ar[3][1][0]))
 
 if __name__ == '__main__':
     exit(main())
